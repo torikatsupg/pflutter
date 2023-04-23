9428fceb8ddcd1b5e2fdfc5128b7f981fd4f4613
torikatsupg <torikatsu923@gmail.com>
2023-04-24 01:59:26 +0900

    install and play skia

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,27 @@ plugins {
 group = "me.torikatsu"
 version = "1.0-SNAPSHOT"
 
+val skArtifact = "skiko-awt-runtime-macos-x64"
+val skVersion = "0.7.18"
+val lwjglVersion = "3.3.1"
+val lwjglNatives = "natives-macos"
+
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    api("org.jetbrains.skiko:${skArtifact}:${skVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+
+    implementation("org.lwjgl:lwjgl:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-glfw:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-opengl:$lwjglVersion")
+    runtimeOnly("org.lwjgl:lwjgl:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-glfw:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-opengl:$lwjglVersion:$lwjglNatives")
 }
 
 tasks.test {
